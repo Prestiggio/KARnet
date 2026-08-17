876f64bb4fc67b7f35413deddf0d03f206dd5fb8
Landry Rakotoarivelo <cto@prestigg.io>
2026-08-17 21:37:04 +0300

    fix: share design

--- apps/developers/app/reference/route.js
+++ apps/developers/app/reference/route.js
@@ -5,7 +5,13 @@ const config = {
   url: '/openapi.json',
   favicon: '/logo.png',
   metaData: {
-    title: 'K@Rnet Developer API Reference'
+    title: 'K@Rnet Developer API Reference',
+    description: 'Available endpoints for interacting with Roman Catholic Church Network in Madagascar',
+    ogDescription: 'Available endpoints for interacting with Roman Catholic Church Network in Madagascar',
+    ogTitle: 'K@Rnet Developer API Reference',
+    ogImage: 'https://developer.katolika.net/api.jpeg',
+    author: 'Prestiggio',
+    date: '2026-08-17'
   }
 }
 export const GET = ApiReference(config)
